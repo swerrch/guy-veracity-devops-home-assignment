e193e6792ef86e7acd0e5601efa96c4d027cf8a3
“swerrch” <swerrch@gmail.com>
2022-10-18 17:40:46 +0300

    initial work with docker compose

---
+++ scripts/run_sqls.sh
@@ -0,0 +1,15 @@
+#/bin/bash!
+
+ENTRY_PATH='/docker-entrypoint-initdb.d'
+POOL_CONF_PATH='/opt/bitnami/pgpool/conf'
+
+echo "local     all             all                         scram-sha-256" >> $POOL_CONF_PATH/pool_hba.conf
+
+PGPASSWORD=adminpassword psql -d postgres -U postgres -h localhost -f $ENTRY_PATH/create_rw_user.sql
+PGPASSWORD=adminpassword psql -d postgres -U postgres -h localhost -f $ENTRY_PATH/create_ro_user.sql
+
+pg_enc -m -f $POOL_CONF_PATH/pgpool.conf -k $POOL_CONF_PATH/.pgpoolkey -i $ENTRY_PATH/users.txt
+
+pgpool -f $POOL_CONF_PATH/pgpool.conf  -k $POOL_CONF_PATH/.pgpoolkey -a $POOL_CONF_PATH/pool_hba.conf reload
+
+# PGPASSWORD=adminpassword psql -d postgres -U postgres -h localhost -c "select usename || ':' || passwd from pg_shadow;" | grep : | xargs -l > $POOL_CONF_PATH/pool_passwd
